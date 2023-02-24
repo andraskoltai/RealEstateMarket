8a5026e4bc7fc65df6d97ba039f4631d6ef3cb2a
Andras Koltai <Andras_Koltai@epam.com>
2023-02-24 17:55:22 +0100

    Added validation attributes

--- RealEstateMarket.Domain/Entities/RealEstate.cs
+++ RealEstateMarket.Domain/Entities/RealEstate.cs
@@ -10,24 +10,33 @@ namespace RealEstateMarket.Domain.Entities
 {
     public class RealEstate
     {
+        [Key]
         public Guid Id { get; set; }
 
+        [Required, MaxLength(100)]
         public string Region { get; set; }
 
+        [Required, MaxLength(100)]
         public string City { get; set; }
 
+        [Required]
         public short ZipCode { get; set; }
 
+        [Required, MaxLength(100)]
         public string StreetName { get; set; }
 
+        [Required]
         public uint HouseNumber { get; set; }
 
         public string? Description { get; set; }
 
+        [Required]
         public uint Price { get; set; }
 
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$")]
         public string? Email { get; set; }
 
+        [StringLength(15, MinimumLength = 8), RegularExpression(@"^\+?\d+$")]
         public string? Phone { get; set; }
     }
 }
