5d902433bc593c0a4650cb236079398953caeb58
Andras Koltai <Andras_Koltai@epam.com>
2023-03-09 14:36:33 +0100

    Pagination instead of getall

--- RealEstateMarket.Application/Interfaces/IRealEstateRepository.cs
+++ RealEstateMarket.Application/Interfaces/IRealEstateRepository.cs
@@ -1,4 +1,5 @@
-﻿using RealEstateMarket.Domain.Entities;
+﻿using RealEstateMarket.Domain;
+using RealEstateMarket.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace RealEstateMarket.Application.Interfaces
 {
     public interface IRealEstateRepository
     {
-        Task<IEnumerable<RealEstate>> GetAllAsync();
+        Task<IEnumerable<RealEstate>> GetAllPagedAsync(PaginationFilter paginationFilter);
         Task InsertAsync(RealEstate realEstate);
         Task<RealEstate> GetByIdAsync(Guid id);
         Task UpdateAsync(RealEstate realEstate);
