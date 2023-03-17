54a8aee551a19d39174f2edffc121e96726d0b1a
Andras Koltai <Andras_Koltai@epam.com>
2023-03-17 13:00:22 +0100

    Result class for validation results

---
+++ RealEstateMarket.Domain/Validation/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateMarket.Domain.Validation
+{
+    public class Result
+    {
+        public bool IsSuccesful => Errors.Count == 0;
+        public List<string> Errors { get; set; } = new List<string>();
+    }
+}
